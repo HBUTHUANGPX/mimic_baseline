85aebd5c5cc8cf6e972508d1c7be00e600e2f7da
huangpeixin <huangpeixin@huaqin.com>
2026-03-15 19:33:33 +0800

    change param

--- general_motion_tracker_whole_body_teleoperation/general_motion_tracker_whole_body_teleoperation/tasks/tracking_q1/teacher_wo_d_tracking_env_cfg.py
+++ general_motion_tracker_whole_body_teleoperation/general_motion_tracker_whole_body_teleoperation/tasks/tracking_q1/teacher_wo_d_tracking_env_cfg.py
@@ -494,7 +494,7 @@ class TerminationsCfg:
     time_out = DoneTerm(func=mdp.time_out, time_out=True)
     ref_pos = DoneTerm(
         func=mdp.bad_ref_pos_z_only,
-        params={"command_name": "motion", "threshold": 0.15},
+        params={"command_name": "motion", "threshold": 0.35},
     )
     ref_ori = DoneTerm(
         func=mdp.bad_ref_ori,
@@ -508,7 +508,7 @@ class TerminationsCfg:
         func=mdp.bad_motion_body_pos_z_only,
         params={
             "command_name": "motion",
-            "threshold": 0.15,
+            "threshold": 0.2,
             "body_names": [
                 "L_ankle_roll_link",
                 "R_ankle_roll_link",
