91c4682c3291ef4ec8dda9820dc88e05349163ae
huangpeixin <huangpeixin@huaqin.com>
2026-04-09 11:11:25 +0800

    分割motion_loader独立cli

--- general_motion_tracker_whole_body_teleoperation/general_motion_tracker_whole_body_teleoperation/tasks/tracking/mdp/commands.py
+++ general_motion_tracker_whole_body_teleoperation/general_motion_tracker_whole_body_teleoperation/tasks/tracking/mdp/commands.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
 import math
-import numpy as np
-import os
 import torch
 from collections.abc import Sequence
 from dataclasses import MISSING
@@ -25,51 +23,9 @@ from isaaclab.utils.math import (
 
 if TYPE_CHECKING:
     from isaaclab.envs import ManagerBasedRLEnv
-
-
-class MotionLoader:
-    def __init__(
-        self, motion_file: str, body_indexes: Sequence[int], device: str = "cpu"
-    ):
-        assert os.path.isfile(motion_file), f"Invalid file path: {motion_file}"
-        data = np.load(motion_file)
-        self.fps = data["fps"]
-        self.joint_pos = torch.tensor(
-            data["joint_pos"], dtype=torch.float32, device=device
-        )
-        self.joint_vel = torch.tensor(
-            data["joint_vel"], dtype=torch.float32, device=device
-        )
-        self._body_pos_w = torch.tensor(
-            data["body_pos_w"], dtype=torch.float32, device=device
-        )
-        self._body_quat_w = torch.tensor(
-            data["body_quat_w"], dtype=torch.float32, device=device
-        )
-        self._body_lin_vel_w = torch.tensor(
-            data["body_lin_vel_w"], dtype=torch.float32, device=device
-        )
-        self._body_ang_vel_w = torch.tensor(
-            data["body_ang_vel_w"], dtype=torch.float32, device=device
-        )
-        self._body_indexes = body_indexes
-        self.time_step_total = self.joint_pos.shape[0]
-
-    @property
-    def body_pos_w(self) -> torch.Tensor:
-        return self._body_pos_w[:, self._body_indexes]
-
-    @property
-    def body_quat_w(self) -> torch.Tensor:
-        return self._body_quat_w[:, self._body_indexes]
-
-    @property
-    def body_lin_vel_w(self) -> torch.Tensor:
-        return self._body_lin_vel_w[:, self._body_indexes]
-
-    @property
-    def body_ang_vel_w(self) -> torch.Tensor:
-        return self._body_ang_vel_w[:, self._body_indexes]
+from general_motion_tracker_whole_body_teleoperation.tasks.tracking.mdp.motion_loader import (
+    MotionLoader,
+)
 
 
 class MotionCommand(CommandTerm):
